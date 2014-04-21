84708c9ecca4fe786a3fb3a9e485b6c8b1160ea8
Ilia <ilia@ilia-Z68P-DS3.(none)>
2014-04-20 22:51:45 -0400

    did mad work

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -9,21 +9,20 @@ class ReservationsController < ApplicationController
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
     @reservation = Reservation.new
-  	@reservation.restaurant = @restaurant
+    @reservation.restaurant = @restaurant
     @reservation.party_size = params[:reservation][:party_size]
     
     new_reserv = DateTime.strptime(params[:reservation][:reservation_date], "%m/%d/%Y").to_date
     @reservation.reservation_date = new_reserv
-
-    #@reservation.reservation_date = params[:reservation][:reservation_date]
     @reservation.rtime = params[:rtime]
     
-    if !check_seats(@reservation.reservation_date, @reservation.rtime, @restaurant.capacity)
+    if !check_seats(@reservation.party_size, @reservation.reservation_date, @reservation.rtime, @restaurant.capacity)
       flash[:alert] = "Oh no! No more seats left."
       redirect_to restaurants_path(@reservation.restaurant.id)
+    else
+      #@reservation.save
+      flash[:alert] = "Reservation made!"
     end
-    @reservation.save
-    flash[:alert] = "Reservation made."
   end
 
   def destroy
@@ -32,12 +31,13 @@ class ReservationsController < ApplicationController
   def show
   end
   private
-  def check_seats(reservation_date, rtime, capacity)
+  def check_seats(party_size, reservation_date, rtime, capacity)
     @reservations = Reservation.where("reservation_date = ?", reservation_date)
     @reservations = @reservations.where("rtime = ?", rtime)
-          reserved_count = @reservations.sum(:party_size)
-          puts reserved_count
-          return false if reserved_count >= capacity
-        else true
+    @reserved_count = @reservations.sum(:party_size) + party_size
+    if @reserved_count >= capacity
+      return false
+    else return true
+    end
   end 
 end
